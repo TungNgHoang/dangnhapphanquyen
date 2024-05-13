f8a873714dfda8777cdb301848ad97aa21fda738
Nguyen HoangTung <tung05122000@gmail.com>
2024-05-13 17:33:03 +0700

    Add project files.

---
+++ Dangnhapphanquyen/Data/Migrations/20240512164102_AddDateOfBirth.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dangnhapphanquyen.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDateOfBirth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
